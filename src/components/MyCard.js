7494e1c2da8551fa4c6c87dc8e5d79531079dd9c
Christopher <kriss17388+dev@gmail.com>
2023-02-10 13:17:40 +0000

    image props

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -18,12 +18,12 @@ function MyCard(props) {
       <Card maxW="sm">
         <CardBody>
           <Image
-            src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+            src={props.image}
             alt="Green double couch with wooden legs"
             borderRadius="lg"
           />
           <Stack mt="6" spacing="3">
-            <Heading size="md">Living room Sofa</Heading>
+            <Heading size="md">{props.title}</Heading>
             <Text>
               {props.desc}
             </Text>
